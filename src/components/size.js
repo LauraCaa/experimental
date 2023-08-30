8efc835403525e7da2dd84ac9f4e8d5145f521fa
Camila <cardenascastrolauracamila7@gmail.com>
2023-08-30 13:58:35 -0500

    feat: New container

---
+++ src/components/size.js
@@ -0,0 +1,31 @@
+export default function Size(){
+    return(
+        <div className="container-fluid d-block vw-100 vh-100 bg-primary">
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+            <h1>hola mundo</h1>
+        </div>
+    )
+}
---
+++ src/pages/wh.js
@@ -0,0 +1,11 @@
+import Size from "@/components/size";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Wh(){
+    return(
+    <>
+        <Size></Size>
+    </>
+    )
+    
+}
