a3ca441701f60ea8b376ede7c78edc71ea33ae9d
Camila <cardenascastrolauracamila7@gmail.com>
2023-09-07 18:33:22 -0500

    feat: Fixed position

---
+++ src/pages/stuck.js
@@ -0,0 +1,10 @@
+import '@/assets/globals.css'
+import StuckObject from '@/components/stuck-object';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default function Stuck(){
+    return( 
+        <>
+            <StuckObject></StuckObject>
+        </>
+    )
+}
