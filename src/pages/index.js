7c83afcaa7fcf3c31dac923fb08b316297f5c14d
Camila <cardenascastrolauracamila7@gmail.com>
2023-09-07 21:00:12 -0500

    fix: Modified index content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
-import Dflex from "@/components/d-flex"
+import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Home() {
-  return (
-    <div>
-      <Dflex></Dflex>
-    </div>
+export default function Home(){
+  return(
+      <nav className="navbar navbar-expand-lg bg-danger-subtle p-4">
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav fw-bold">
+            <li className="nav-item">
+              <Link className="nav-link" href="/navbar">Navbar</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/position">Position</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/stuck">Stuck</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="/dflex">D-flex</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
   )
 }
